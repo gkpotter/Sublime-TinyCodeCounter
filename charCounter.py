03169146221394cc1a7f4c09bfa327bf9b0751f7
gkpotter <gkp@bu.edu>
2021-01-24 19:14:39 -0500

    Added copied message which displays for 1 second and implemented more settings.

--- charCounter.py
+++ charCounter.py
@@ -2,9 +2,10 @@ import sublime
 import sublime_plugin
 
 settings = sublime.load_settings('TinyCodeCounter.sublime-settings')
-languages = settings.get('languages')
 
 class charCounter(sublime_plugin.ViewEventListener):
+	languages = settings.get('languages')
+	
 	def __init__(self, view):
 		self.view = view
 		self.phantom_set = sublime.PhantomSet(view)
@@ -14,13 +15,17 @@ class charCounter(sublime_plugin.ViewEventListener):
 		self.remove_newlines = settings.get('remove_newlines')
 		self.colors = settings.get('colors')
 		self.label = settings.get('label')
-		
-		self.update()
+		self.zero_char = settings.get('zero_char')
+		self.include_marker_line = settings.get('include_marker_line')
+
+		self.just_copied = False
 
 	@classmethod
 	def is_applicable(cls, settings):
 		syntax = settings.get('syntax')
+		languages = cls.languages
 		return any(language in syntax for language in languages)
+
 
 	def update(self):
 		phantoms = []
@@ -45,29 +50,28 @@ class charCounter(sublime_plugin.ViewEventListener):
 			if self.remove_newlines:
 				code = code.replace('\n', '')
 
-			# do not remove whitespace from comment
-			comment = self.view.substr(sublime.Region(marker_start, code_region.b)).strip()
-			code += comment
+			if self.include_marker_line:
+				# do not remove whitespace from marker_line except from the end
+				marker_line = self.view.substr(sublime.Region(marker_start, code_region.b)).strip()
+				code += marker_line
 
 			count = len(code)
 			limit = self.char_limit
-			diff = count-limit
+			diff = count - limit
 
 			color = self.view.style_for_scope('comment')['foreground']
+			offset_color = self.colors['over'] if count > limit else self.colors['under']
 
-			style = ('<style>a{text-decoration:none;color:'
-				+ str(color)
-				+ '}#offset{color:'
-				+ (self.colors['over'] if count > limit else self.colors['under'] ) 
-				+ '}</style>')
-			
-			text = ('<a href=copy>'
-				+ self.label
-				+	str(count)
-				+ ' (<span id="offset">'
-				+ ('+' if diff > 0 else '' if diff == 0 else '-')
-				+ str(abs(diff))
-				+ '</span>)</a>')
+			style = '<style> a {{ text-decoration: none; color: {} }} #offset {{ color: {} }} </style>'.format(
+				color, 
+				offset_color) 
+
+			text = '<a href>{} {} (<span id="offset">{}{}</span>){}</a>'.format(
+				self.label, 
+				count, 
+				'+' if diff > 0 else self.zero_char if diff == 0 else '-', 
+				abs(diff) if diff != 0 else '',
+				' Copied!' if self.just_copied else '')
 
 			phantoms.append(sublime.Phantom(
 				sublime.Region(marker_start),
@@ -75,12 +79,22 @@ class charCounter(sublime_plugin.ViewEventListener):
 				sublime.LAYOUT_BLOCK,
 				lambda href: self.set_clipboard(code)))
 
+		self.just_copied = False
 		self.phantom_set.update(phantoms)
 
+
 	def set_clipboard(self, code):
-		print('Copied!')
-		sublime.set_clipboard(code)
+		if not self.just_copied:
+			sublime.set_timeout(self.update, 1000)
+			
+			sublime.set_clipboard(code)
+			self.just_copied = True
+			self.update()
+
 
 	def on_modified(self):
 		self.update()
-			
+
+
+	def on_load_async(self):
+		self.update()
